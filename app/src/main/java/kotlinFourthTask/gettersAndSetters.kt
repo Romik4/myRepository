ce571166e43793c0d20fb84dd58eedbafb3f30e1
Roman Pereguda <pereguda@surf.dev>
2024-02-18 13:51:26 +0300

    Блок 4 Часть 1: ООП

---
+++ app/src/main/java/kotlinFourthTask/gettersAndSetters.kt
@@ -0,0 +1,38 @@
+package kotlinFourthTask
+
+/**
+ *      Задание 5
+ *
+ *      В классе Person добавьте custom геттеры и сеттеры для свойств name и age.
+ *      Убедитесь, что вы можете управлять доступом к этим свойствам извне класса.
+ *      Например, вы можете реализовать проверку возраста в сеттере,
+ *      чтобы убедиться, что возраст неотрицательный.
+ */
+
+fun main() {
+    /**
+     * Решил сделать так, чтобы пользователь мог вбить в консоль возраст,
+     * после чего в сеттере будет проходить проверка на возраст
+     */
+    romik.age = enterAge
+    println(romik.name)
+
+}
+
+val enterNum = java.util.Scanner(System.`in`)
+val enterAge = enterNum.nextInt()
+
+class PersonTwo {
+    var name: String = "Рома"
+        get() = "Ромик"
+
+    var age: Int = 24
+        set(value: Int) {
+            field = value
+            if (field > 0) println("возраст положительный: $field") else println("возраст не может быть отрицательным")
+        }
+
+
+}
+
+var romik = PersonTwo()
