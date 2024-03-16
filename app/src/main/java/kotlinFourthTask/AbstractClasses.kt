515c6e21ebf18690fbe2f3c66e5adda481b09424
Roman Pereguda <pereguda@surf.dev>
2024-03-16 20:08:58 +0300

    Блок 4 Часть 1: ООП

--- app/src/main/java/kotlinFourthTask/AbstractClasses.kt
+++ app/src/main/java/kotlinFourthTask/AbstractClasses.kt
@@ -9,46 +9,25 @@ package kotlinFourthTask
  */
 
 fun main() {
-    circle.area()
-    rectangle.area()
+    val circle = Circle()
+    val rectangle = Rectangle()
 
-//    println(circle.area())
-//    println(rectangle.area())
+    println(circle.area())
+    println(rectangle.area())
 }
 
 abstract class Shape {
-    abstract fun area()//: Double
+    abstract fun area(): Number
 }
 
-/**
- * Оказалось, что если у абстрактного метода указать возвращаемый тип данных,
- * то мы не сможем его переписать, как минимум я пока не знаю, как это сделать.
- */
-//class Circle: Shape() {
-//    override fun area(): Double {
-//        return 3.14 * 2 * 2
-//    }
-//}
-//
-//class Rectangle: Shape() {
-//    override fun area(): Double {
-//        return 4.15 * 5.20 / 2
-//    }
-//}
-/**
- * А потом я догадался сделать задачу без указания возвращаемого типа данных :)
- */
 class Circle : Shape() {
-    override fun area() {
-        println(3.14 * 2 * 2)
+    override fun area(): Number {
+        return 3.14 * 2 * 2
     }
 }
 
 class Rectangle : Shape() {
-    override fun area() {
-        println(10 * 12 / 2)
+    override fun area(): Number {
+        return 4 * 5 / 2
     }
 }
-
-var circle = Circle()
-var rectangle = Rectangle()
---
+++ app/src/main/java/kotlinFourthTask/sealedClasses.kt
@@ -0,0 +1,18 @@
+package kotlinFourthTask
+
+/**
+ *      Задание 11
+ *
+ *      Создайте sealed class MathOperation и несколько объектов,
+ *      представляющих различные математические операции
+ *      (например, Add, Subtract, Multiply, Divide).
+ *      Создайте функцию, которая принимает MathOperation и два числа,
+ *      и выполняет соответствующую операцию.
+ */
+
+fun main() {
+
+}
+
+sealed class MathOperation
+
