18d800833146cb423f13e5c51bc6cb52394003ca
Roman Pereguda <pereguda.roman220@gmail.com>
2024-05-10 14:07:07 +0300

    Null-безопасность и обработка исключений Фикс последнего задания

--- app/src/main/java/kotlinSixthTask/fifthTask.kt
+++ app/src/main/java/kotlinSixthTask/fifthTask.kt
@@ -10,12 +10,27 @@ package kotlinSixthTask
 
 class InvalidUserInputException(message: String) : Exception(message)
 
+/**
+ *  Чат подсказал использовать регулярное выражение для проверки вводимого числа,
+ *  когда я пытался прописать какие-то условие, у меня постоянно были либо ошибки,
+ *  либо пользовательское исключение просто не работало.
+ *  Самому логику работы я понял, нам необходимо кинуть исключение в блоке try,
+ *  после чего поймать его и обработать.
+ */
+fun isNum(input: String) {
+    if (!input.matches("\\d+".toRegex())) {
+        throw InvalidUserInputException("Исключение")
+    } else {
+        println(input)
+    }
+}
+
 fun main() {
     try {
         println("Введите целое число")
-        val num: Int? = readlnOrNull()?.toInt()
-        println(num)
-    } catch (e: Exception) {
-        throw InvalidUserInputException("Исключение")
+        val input = readlnOrNull() ?: ""
+        isNum(input)
+    } catch (e: InvalidUserInputException) {
+        println(e.message)
     }
 }
